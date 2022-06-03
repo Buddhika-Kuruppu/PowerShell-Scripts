b3136066258c68475bfb79f1d91096ac5fde4f7f
Buddhike Kuruppu <67339257+Buddhika-Kuruppu@users.noreply.github.com>
2022-06-03 23:10:16 +0530

    Create Get-ServiceStatus.ps1
    Initial v1.0

---
+++ Get-ServiceStatus.ps1
@@ -0,0 +1,32 @@
+# Display status of services which are running on a specified machine
+
+# Creates Parameter
+
+Param (
+    [Parameter(Mandatory=$true)]
+    [String[]]  #Accepts Multiple inputs
+     $ComputerName
+)
+
+# Variable for storing Get-Service objects
+
+$Services = Get-Service -ComputerName $ComputerName
+
+# Perform on each objects in Get-Services
+
+Foreach ($Service in $Services){
+
+    $ServiceStatus = $service.Status # Variable for status and display name with member enumeration
+
+    $ServiceDisplayName = $Service.DisplayName
+
+    # Desicion Making
+
+    if ($ServiceStatus -eq 'Running'){
+        Write-Output "Service Fine - Status of $ServiceDisplayName is $ServiceStatus"
+    }
+
+    Else {
+        Write-Output "Check the Service - Status of $ServiceDisplayName is $ServiceStatus"
+    }
+}
